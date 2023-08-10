e8e5d465598b3d45927d2fde52db38b5c693a59e
Lauren <lauren.main28@gmail.com>
2023-08-09 22:08:53 -0700

    starts linked list review

---
+++ README.md
@@ -0,0 +1,36 @@
+### Project: Pythonisms
+
+Author: Lauren Main
+
+Version:  1.0
+
+#### Links and Resources
+
+
+
+#### Overview
+
+##### Features and Tasks
+
+-[x] 
+
+    -[x] 
+
+-[x] 
+
+-[] 
+
+-[] 
+
+
+#### To initialize
+
+Type into your terminal:
+
+`python3.11 -m venv .venv`
+
+`source .venv/bin/activate`
+
+`python caesar_cipher.py`
+
+#### Tests
---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+class Node:
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
+
+    def __str__(self):
+        return str(self.val)
+
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def append(self, val):
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+
+    def __len__(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current.val
+            current = current.next
+
+    def to_list(self):
+        return [value for value in self]
